fdf510d45421a8676751d3d79fe13761ffdcf9ab
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-02-04 10:29:27 -0300

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,30 @@
+algoritmo
+declare X[5], n , i , aux numérico
+// carregando os números no vetor
+for i +- until  4 faça
+  init
+  printf "Digite o ", i+l, " 0 número :
+  printf X[i]
+fim
+// ordenando de forma crescente
+// laço com a quantidade de elementos do vetor
+para n +- 1 até 5 faça
+início
+// laço que percorre da primeira à
+// penúltima posição do vetor
+para i +- O até 3 faça
+  início
+  if (X[i] > X[i+ ll)
+então início
+end
+end
+aux +- X[i]
+X[i] +- X[i+l]
+X[i+l] +- aux
+end
+// mostrando o vetor ordenado
+for i +- O até 4 faça
+  início
+  printf i+l," 0 número : " , X[i]
+end
+fim_alg o ritmo . 
